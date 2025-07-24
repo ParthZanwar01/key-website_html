912f53ff84098d2a6e79ed35d4b2d8ff025b6e17
TheMathGenius01 <parth.zanwar01@gmail.com>
2025-07-23 21:00:27 -0500

    Update HourRequestScreen.js

--- screens/HourRequestScreen.js
+++ screens/HourRequestScreen.js
@@ -25,7 +25,7 @@ import * as FileSystem from 'expo-file-system';
 // Updated Google Apps Script Service for direct folder upload
 class SimpleGoogleDriveService {
   // Google Apps Script Web App URL for photo uploads
-  static APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxmCIbh-YQS5G2oFMqLFGa8LXZwwi4nkTBksAbdAvhlW_EaokITIbUcq3qAkga_hmxt/exec'
+  static APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxaT-nD7E1seV6TH3PcLHG7hsb0k3s9VS97wCGinBvz11rGUbWK3C49a6e1mDyITN4qkA/exec'
   
   static async uploadImage(imageUri, studentNumber, eventName) {
     try {
