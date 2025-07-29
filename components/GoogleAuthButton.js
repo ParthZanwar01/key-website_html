c8f0fec7f2d364509cab60519a7392f3215b1266
TheMathGenius01 <parth.zanwar01@gmail.com>
2025-07-29 16:38:33 -0500

    help

--- components/GoogleAuthButton.js
+++ components/GoogleAuthButton.js
@@ -31,11 +31,6 @@ const GoogleAuthButton = ({ onAuthSuccess, style }) => {
         revocationEndpoint: GoogleOAuthService.REVOKE_ENDPOINT,
       };
       
-      // Store the code verifier for PKCE
-      if (request.codeVerifier) {
-        localStorage.setItem('oauth_code_verifier', request.codeVerifier);
-      }
-      
       // Create the authorization URL
       const authUrl = await request.makeAuthUrlAsync(discovery);
       
