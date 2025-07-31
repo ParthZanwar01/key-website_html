cdcd384f375754532dc99c1d2f8029bd2a698a73
TheMathGenius01 <parth.zanwar01@gmail.com>
2025-07-31 16:09:21 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -807,7 +807,7 @@ export default function HomeScreen() {
               </TouchableOpacity>
             </View>
           </Animated.View>
-        </Animated.View>
+        </View>
       </Modal>
     </SafeAreaView>
   );
